987f5d261319f2e5724ed089dbd876f29b0afc4e
Amit8281 <ak15072419@gmail.com>
2023-07-23 15:58:20 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Kumar",
+    author_email="ak15072419@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
